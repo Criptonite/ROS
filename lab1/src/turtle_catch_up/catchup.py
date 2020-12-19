993d31975e32866e35d186fe0b1247aca0d220c0
Guba Dmitriy <gudmian@gmail.com>
2020-12-20 02:07:20 +0300

    fix bugs

--- lab1/src/turtle_catch_up/catchup.py
+++ lab1/src/turtle_catch_up/catchup.py
@@ -7,15 +7,15 @@ from turtlesim.msg import Pose
 class catchuper:
     def __init__(self):
         self.pose = Pose()
-        self.pub = rospy.Publisher(str(rospy.get_param) + '/cmd_vel', Twist, queue_size=1)
-        self.sub1 = rospy.Subscriber('', Pose, self.catch_up)
-        self.sub2 = rospy.Subscriber('', Pose, self.refresh_pose)
+        self.pub = rospy.Publisher('donatello/cmd_vel', Twist, queue_size=1)
+        self.sub1 = rospy.Subscriber('turtle1/pose', Pose, self.catch_up)
+        self.sub2 = rospy.Subscriber('donatello/pose', Pose, self.refresh_pose)
 
     def catch_up(self, turtlePos):
         msg = Twist()
         rotate = math.atan2(turtlePos.y - self.pose.y, turtlePos.x - self.pose.x)
-        msg.angular.z = angle - self.pose.theta
-		msg.linear.x = turtlePos.linear_velocity
+        msg.angular.z = rotate - self.pose.theta
+	msg.linear.x = turtlePos.linear_velocity
         self.pub.publish(msg)
 
     def refresh_pose(self, turtlePos):
